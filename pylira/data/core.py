650dc56f2ce4ea170d0d0859e02f92b770cc63df
Axel Donath <axel.donath@mpi-hd.mpg.de>
2021-11-09 17:05:04 -0500

    Implement point_source_gauss_psf test data

--- pylira/data/core.py
+++ pylira/data/core.py
@@ -2,15 +2,32 @@ import numpy as np
 from astropy.convolution import Gaussian2DKernel, convolve_fft
 
 
-__all__ = ["get_point_source_gauss_psf"]
+__all__ = ["point_source_gauss_psf"]
 
 
-def get_point_source_gauss_psf(sigma_psf=3):
-    """Get point source with Gaussian PSF test data
+def point_source_gauss_psf(
+        shape=(32, 32),
+        shape_psf=(16, 16),
+        sigma_psf=3,
+        source_level=1000,
+        background_level=2,
+):
+    """Get point source with Gaussian PSF test data.
+
+    The exposure is assumed to be constant.
 
     Parameters
     ----------
-
+    shape : tuple
+        Shape of the data array.
+    shape_psf : tuple
+        Shape of the psf array.
+    sigma_psf : float
+        Width of the psf in pixels.
+    source_level : float
+        Total integrated counts of the source
+    background_level : float
+        Background level in counts / pixel.
 
     Returns
     -------
@@ -18,26 +35,21 @@ def get_point_source_gauss_psf(sigma_psf=3):
         Data dictionary
     """
     np.random.seed(836)
-    shape, shape_psf = (32, 32), (16, 16)
 
-    psf = Gaussian2DKernel(sigma_psf, x_size=shape_psf[1], y_size=shape_psf[1]).array
+    background = background_level * np.ones(shape)
 
-    alpha_init = np.ones(psf.shape[0])
-
-    background = 2 * np.ones(shape)
     signal = np.zeros(shape)
-    signal[shape[0] // 2, shape[1] // 2] = 1000
+    signal[shape[0] // 2, shape[1] // 2] = source_level
 
+    psf = Gaussian2DKernel(sigma_psf, x_size=shape_psf[1], y_size=shape_psf[1])
     npred = background + convolve_fft(signal, psf)
 
     counts = np.random.poisson(npred)
     exposure = np.ones(shape)
     return {
-        "observed_im": counts.astype(np.float64),
-        "start_im": npred.astype(np.float64),
-        "psf_im": psf.astype(np.float64),
-        "expmap_im": exposure.astype(np.float64),
-        "baseline_im": background.astype(np.float64),
-        "alpha_init": alpha_init.astype(np.float64),
+        "counts": counts,
+        "psf": psf,
+        "exposure": exposure,
+        "background": background,
     }
 
