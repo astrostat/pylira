160320bd0cd072a8f798d22dc013254050d5084d
Axel Donath <axel.donath@mpi-hd.mpg.de>
2021-11-16 12:15:19 -0500

    Add disk source example dataset

--- pylira/data/core.py
+++ pylira/data/core.py
@@ -1,8 +1,8 @@
 import numpy as np
-from astropy.convolution import Gaussian2DKernel, convolve_fft
+from astropy.convolution import Gaussian2DKernel, Tophat2DKernel, convolve_fft
 
 
-__all__ = ["point_source_gauss_psf"]
+__all__ = ["point_source_gauss_psf", "disk_source_gauss_psf"]
 
 
 def point_source_gauss_psf(
@@ -41,6 +41,60 @@ def point_source_gauss_psf(
 
     flux = np.zeros(shape)
     flux[shape[0] // 2, shape[1] // 2] = source_level
+
+    psf = Gaussian2DKernel(sigma_psf, x_size=shape_psf[1], y_size=shape_psf[1])
+    npred = background + convolve_fft(flux * exposure, psf)
+
+    counts = np.random.poisson(npred)
+    return {
+        "counts": counts,
+        "psf": psf.array,
+        "exposure": exposure,
+        "background": background,
+        "flux": flux
+    }
+
+
+def disk_source_gauss_psf(
+        shape=(32, 32),
+        shape_psf=(17, 17),
+        sigma_psf=3,
+        source_level=1000,
+        source_radius=3,
+        background_level=2,
+):
+    """Get disk source with Gaussian PSF test data.
+
+    The exposure has a gradient of 50% from left to right.
+
+    Parameters
+    ----------
+    shape : tuple
+        Shape of the data array.
+    shape_psf : tuple
+        Shape of the psf array.
+    sigma_psf : float
+        Width of the psf in pixels.
+    source_level : float
+        Total integrated counts of the source
+    source_radius : float
+        Radius of the disk source
+    background_level : float
+        Background level in counts / pixel.
+
+    Returns
+    -------
+    data : dict of `~numpy.ndarray`
+        Data dictionary
+    """
+    np.random.seed(836)
+
+    background = background_level * np.ones(shape)
+    exposure = np.ones(shape) + 0.5 * np.linspace(-1, 1, shape[0])
+
+    flux = source_level * Tophat2DKernel(
+        radius=source_radius, x_size=shape[1], y_size=shape[1], mode="oversample"
+    ).array
 
     psf = Gaussian2DKernel(sigma_psf, x_size=shape_psf[1], y_size=shape_psf[1])
     npred = background + convolve_fft(flux * exposure, psf)
--- pylira/tests/test_core.py
+++ pylira/tests/test_core.py
@@ -1,7 +1,7 @@
 import numpy as np
 from numpy.testing import assert_allclose
 import pylira
-from pylira.data import point_source_gauss_psf
+from pylira.data import point_source_gauss_psf, disk_source_gauss_psf
 from pylira import LIRADeconvolver
 
 
@@ -19,7 +19,16 @@ def test_data_point_source_gauss_psf():
     assert_allclose(data["counts"][0][0], 2)
     assert_allclose(data["exposure"][0][0], 1)
     assert_allclose(data["background"][0][0], 2.)
-    assert_allclose(data["psf"][0][0], 1.4429833050509627e-05, rtol=1e-5)
+    assert_allclose(data["psf"][0][0], 1.44298e-05, rtol=1e-5)
+
+
+def test_data_disk_source_gauss_psf():
+    data = point_disk_gauss_psf()
+
+    assert_allclose(data["counts"][0][0], 2)
+    assert_allclose(data["exposure"][0][0], 1)
+    assert_allclose(data["background"][0][0], 2.)
+    assert_allclose(data["psf"][0][0], 1.44298e-05, rtol=1e-5)
 
 
 def test_lira_deconvolver():
@@ -31,7 +40,6 @@ def test_lira_deconvolver():
     assert_allclose(deconvolve.alpha_init, [1., 2., 3.])
 
 
-# TODO: this still fails with a segfault...
 def test_lira_deconvolver_run():
     data = point_source_gauss_psf()
     data["flux_init"] = data["flux"]
