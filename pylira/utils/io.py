e0362f3f060ea378527011a2e3a20570662865a0
Axel Donath <axel.donath@mpi-hd.mpg.de>
2021-11-24 14:28:41 -0500

    Implement write_to_fits_hdulist

--- pylira/utils/io.py
+++ pylira/utils/io.py
@@ -1,5 +1,6 @@
 import numpy as np
 from astropy.table import Table
+from astropy.io import fits
 
 
 def read_parameter_trace_file(filename):
@@ -41,3 +42,39 @@ def read_image_trace_file(filename):
     n_iter = shape_y // shape_x
 
     return data.reshape((n_iter, shape_x, shape_x))
+
+
+def write_to_fits_hdulist(result, filename, overwrite):
+    """Convert LIRA result to list of FITS hdus.
+
+    Parameters
+    ----------
+    result : `LIRADeconvolverResult`
+        Deconvolution result.
+    filename : `Path`
+        Output filename
+    overwrite : bool
+        Overwrite file.
+    """
+    hdulist = fits.HDUList()
+
+    if result.wcs:
+        header = result.wcs.to_header()
+    else:
+        header = None
+
+    primary_hdu = fits.PrimaryHDU(header=header, data=result.posterior_mean)
+    parameter_trace_hdu = fits.BinTableHDU(result.parameter_trace)
+    image_trace_hdu = fits.ImageHDU(header=header, data=result.image_trace)
+
+    hdulist["POSTERIOR_MEAN"] = primary_hdu
+    hdulist["PARAMETER_TRACE"] = parameter_trace_hdu
+    hdulist["IMAGE_TRACE"] = image_trace_hdu
+
+    with filename.open("w") as f:
+        hdulist.writeto(f, overwrite=overwrite)
+
+
+IO_FORMATS = {
+    "fits": write_to_fits_hdulist
+}
