fd974ae91c7360d7cdcc6d58d85982a4629f64c8
Axel Donath <axel.donath@mpi-hd.mpg.de>
2022-03-07 16:08:04 -0500

    Adapt test values

--- pylira/utils/tests/test_priors.py
+++ pylira/utils/tests/test_priors.py
@@ -9,11 +9,11 @@ def test_f_hyperprior_esch():
         index=0,
         index_alpha=3
     )
-    assert_allclose(value, 1)
+    assert_allclose(value, 0.000335, atol=1e-5)
 
 
 def test_f_hyperprior_lira():
     value = f_hyperprior_lira(
         alpha=0.2, ms_al_kap1=0, ms_al_kap2=1000, ms_al_kap3=3
     )
-    assert_allclose(value, 1)
+    assert_allclose(value, 0.000335, atol=1e-5)
