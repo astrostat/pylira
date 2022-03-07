1f508deeff464e60b3385e365cd7d1c4983b3063
Axel Donath <axel.donath@mpi-hd.mpg.de>
2022-03-07 15:56:06 -0500

    Add prior tests

---
+++ pylira/utils/tests/test_priors.py
@@ -0,0 +1,19 @@
+from numpy.testing import assert_allclose
+from pylira.utils.priors import f_hyperprior_esch, f_hyperprior_lira
+
+
+def test_f_hyperprior_esch():
+    value = f_hyperprior_esch(
+        alpha=0.2,
+        delta=3000,
+        index=0,
+        index_alpha=3
+    )
+    assert_allclose(value, 1)
+
+
+def test_f_hyperprior_lira():
+    value = f_hyperprior_lira(
+        alpha=0.2, ms_al_kap1=0, ms_al_kap2=1000, ms_al_kap3=3
+    )
+    assert_allclose(value, 1)
