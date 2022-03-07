f9dbb41223f0040444792431704e9edbe6b9f52d
Axel Donath <axel.donath@mpi-hd.mpg.de>
2022-03-07 14:52:54 -0500

    Add hyperprior distributions

---
+++ pylira/utils/priors.py
@@ -0,0 +1,53 @@
+import numpy as np
+
+
+def f_hyperprior_esch(alpha, delta=1, index_alpha=1, index=0):
+    """Hyperprior distribution following Esch et. al. 2004
+
+    Parameters
+    ----------
+    alpha : float or `~numpy.ndarray`
+        Alpha parameter
+    delta : float or `~numpy.ndarray`
+        Delta parameter
+    index_alpha : float or `~numpy.ndarray`
+        Index alpha parameter
+    index : float or `~numpy.ndarray`
+        Index parameter
+
+    Returns
+    -------
+    value : `~numpy.ndarray`
+        Value
+    """
+    prefactor = (alpha * delta) ** index
+    exponential = np.exp(-delta * alpha ** index_alpha / index_alpha)
+    return prefactor * exponential
+
+
+def f_hyperprior_lira(alpha, ms_al_kap1, ms_al_kap2, ms_al_kap3):
+    """Hyperprior distribution following LIRA implementation
+
+    Parameters
+    ----------
+    alpha : float or `~numpy.ndarray`
+        Alpha parameter
+    ms_al_kap1: float or `~numpy.ndarray`
+        Multiscale prior parameter.
+    ms_al_kap2: float or `~numpy.ndarray`
+        Multiscale prior parameter.
+    ms_al_kap3: float or `~numpy.ndarray`
+        Multiscale prior parameter.
+
+    Returns
+    -------
+    value : `~numpy.ndarray`
+        Value
+
+    """
+    delta = ms_al_kap2 * ms_al_kap3
+    index = ms_al_kap1
+    index_alpha = ms_al_kap3
+    return f_hyperprior_esch(alpha=alpha, delta=delta, index=index, index_alpha=index_alpha)
+
+
